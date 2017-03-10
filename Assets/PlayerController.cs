b7da44160fac6510dec7af6cf8ebde4da9386188
Matthew Consterdine <mrconsterdine@googlemail.com>
2017-03-10 19:42:32 +0000

    Add movement prototype

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 5;
+
+	private Rigidbody _rb;
+
+	public void Start () {
+		_rb = GetComponent<Rigidbody>();
+	}
+
+	public void OnMoveHorizontal(float value) {
+		_rb.AddForce(-transform.right * value * speed);
+	}
+
+	public void OnMoveVertical(float value) {
+		_rb.AddForce(transform.forward * value * speed);
+	}
+}
