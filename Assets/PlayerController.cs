bcf5cf3674f4ed6fef258defc572003e6d437f95
Matthew Consterdine <mrconsterdine@googlemail.com>
2017-03-11 21:21:42 +0000

    Rotate player model when moving

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,19 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	public float speed = 5;
+	public float speed    = 5;
+	public float rotation = 0.5f;
 
+	private Vector2 _rotation = new Vector2(0, 0);
 	private Rigidbody _rb;
 
 	public void Start () {
 		_rb = GetComponent<Rigidbody>();
 	}
 
+	public void Update() {
+		// Rotate smoothly to the desired angle.
+		transform.rotation = Quaternion.Slerp(
+			transform.rotation,
+			Quaternion.Euler(new Vector3(0, (Mathf.PI + Mathf.Atan2(_rotation.x, _rotation.y)) * Mathf.Rad2Deg, 0)),
+			rotation
+		);
+	}
+
 	public void OnMoveHorizontal(float value) {
-		_rb.AddForce(-transform.right * value * speed);
+		if(value != 0) {
+			_rb.AddForce(Vector3.right * -value * speed);
+			_rotation.x = -value;
+		}
 	}
 
 	public void OnMoveVertical(float value) {
-		_rb.AddForce(transform.forward * value * speed);
+		if(value != 0) {
+			_rb.AddForce(Vector3.forward * value * speed);
+			_rotation.y = value;
+		}
 	}
 }
