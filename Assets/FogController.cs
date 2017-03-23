6e31de6ebd940c9d04bf9ad6d9ca757b72c36f62
Matthew Consterdine <mrconsterdine@googlemail.com>
2017-03-23 16:06:00 +0000

    Add day night cycle

---
+++ Assets/FogController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogController : MonoBehaviour {
+	public Color darkColor  = Color.black;
+	public Color lightColor = Color.white;
+
+	public float darkDensity  = 0.02f;
+	public float lightDensity = 0.01f;
+
+	public void Update () {
+		float position = Mathf.Max(0, transform.position.y/500f);
+		RenderSettings.fogColor = Color.Lerp(darkColor, lightColor, position);
+		RenderSettings.fogDensity = Mathf.Lerp(darkDensity, lightDensity, position);
+	}
+}
---
+++ Assets/SunController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunController : MonoBehaviour {
+	public void FixedUpdate() {
+		transform.RotateAround(Vector3.zero, Vector3.right, 10f*Time.deltaTime);
+		transform.LookAt(Vector3.zero);
+	}
+}
