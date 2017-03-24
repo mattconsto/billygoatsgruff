112dc09483a2d455adee97347e1ecfe3738b4a14
Matthew Consterdine <mrconsterdine@googlemail.com>
2017-03-24 23:40:39 +0000

    Add goat pathfinding

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GameController : MonoBehaviour {
 	public string message;
@@ -11,8 +12,33 @@ public class GameController : MonoBehaviour {
 
 	public GameObject dynamicCamera;
 	
+	private float _switchTimer = 0;
+
+	public GameObject[] players; 
+	private int _pointer = 0;
+
 	public void Update () {
-		
+		_switchTimer -= Time.deltaTime;
+
+		if(Input.GetButton("Fire1") && _switchTimer <= 0) {
+			_switchTimer = 0.25f;
+			foreach(GameObject player in players) {
+				player.GetComponent<NavMeshAgent>().Stop();
+				player.GetComponent<PlayerController>().enabled = false;
+			}
+			_pointer = (_pointer + 1) % players.Length;
+			players[_pointer].GetComponent<PlayerController>().enabled = true;
+		}
+
+		if(Input.GetButton("Cancel")) {
+			for(int i = 0; i < players.Length; i++) {
+				if(i == _pointer) continue;
+
+				NavMeshAgent agent = players[i].GetComponent<NavMeshAgent>();
+				agent.SetDestination(players[_pointer].transform.position);
+				agent.Resume();
+			}
+		}
 	}
 
 	public void SetMessage(string message, float time) {
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,18 +22,16 @@ public class PlayerController : MonoBehaviour {
 		if(_rotation.magnitude > 0.1) {
 			transform.rotation = Quaternion.Slerp(
 				transform.rotation,
-				Quaternion.Euler(new Vector3(0, (Mathf.PI + Mathf.Atan2(_rotation.x, _rotation.y)) * Mathf.Rad2Deg, 0)),
+				Quaternion.Euler(new Vector3(0, (Mathf.PI + Mathf.Atan2(-_rotation.x, -_rotation.y)) * Mathf.Rad2Deg, 0)),
 				rotation
 			);
 		}
 
 		// Minimise strafing
-		if(_canJump > 0) {
-			_rb.AddForce(Vector3.right * _rotation.x * speed);
-			_rb.AddForce(Vector3.forward * _rotation.y * speed);
-		}
+		_rb.AddForce(Vector3.right * _rotation.x * speed * (_canJump > 0 ? 1f : 0.5f));
+		_rb.AddForce(Vector3.forward * _rotation.y * speed * (_canJump > 0 ? 1f : 0.5f));
 
-		if (Input.GetAxis("Jump") > 0 && _canJump > 0) {
+		if(Input.GetButton("Jump") && _canJump > 0) {
 			_rb.AddForce(transform.up * jump);
 			_rb.AddForce(-transform.forward * jump);
 		}
