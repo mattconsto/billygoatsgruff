d0e66ec3c0053183c050332911a6fb890b018ac9
Matthew Consterdine <mrconsterdine@googlemail.com>
2017-04-20 16:00:32 +0100

    Improve pathing and water

---
+++ Assets/Scripts/BridgeReset.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeReset : MonoBehaviour {
+	void Update () {
+		transform.rotation = Quaternion.identity;
+		enabled = false;
+	}
+}
