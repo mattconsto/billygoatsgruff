7de581760e4bfcfc713a61e8412ca804477d8297
Matthew Consterdine <mrconsterdine@googlemail.com>
2017-04-21 14:17:23 +0100

    Add lens flare

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour {
+	public GameController controller;
+
+	public void OnTriggerEnter() {
+		controller.End();
+	}
+}
